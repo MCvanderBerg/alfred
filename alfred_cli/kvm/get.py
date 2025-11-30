252b5cc4bb917e8184818d74cfa673312198f2e1
Christiaan van der Berg <christiaan.vanderberg@comotion.co.za>
2025-12-01 00:19:22 +0200

    fixed kvm list

--- alfred_cli/kvm/get.py
+++ alfred_cli/kvm/get.py
@@ -2,18 +2,24 @@ import click
 import json
 from importlib.resources import files
 
+
 @click.command()
-@click.argument('key')
+@click.argument("key")
 def get(key):
-    with (files("alfred_cli.kvm") / "data.json").open() as f:
-        data = json.load(f)
+    """Get a value by key from the backing store."""
+    data_path = files("alfred_cli.kvm") / "data.json"
+
+    try:
+        with data_path.open() as f:
+            data = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        click.echo("no data store found yet")
+        raise SystemExit(1)
 
     try:
         value = data[key]
     except KeyError:
-            click.echo(f"no value associated to this key {key}")
-            raise SystemExit(1)
+        click.echo(f"no value associated to this key {key}")
+        raise SystemExit(1)
 
     click.echo(value)
-
-
--- alfred_cli/kvm/list.py
+++ alfred_cli/kvm/list.py
@@ -2,8 +2,20 @@ import click
 import json
 from importlib.resources import files
 
+
 @click.command()
 def list():
-    with (files("alfred_cli.kvm") / "data.json").open() as f:
-        data = json.load(f)
-        click.echo(data)
+    """List all stored key/value pairs.
+
+    Falls back to an empty mapping if the backing store doesn't exist yet
+    or is unreadable.
+    """
+    data_path = files("alfred_cli.kvm") / "data.json"
+
+    try:
+        with data_path.open() as f:
+            data = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        data = {}
+
+    click.echo(data)
